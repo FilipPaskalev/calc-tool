0d1767f8676721173f9a47912179b71cdef362a0
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-21 16:46:13 +0100

    set GitHub pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './styles/App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={Home} exact />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
